b99dc8226723e76a4eb06accb9496890088beb2c
kaiz3n08 <yagnik9922@gmail.com>
2026-08-08 01:50:02 +0530

    job get point

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { type FastifyInstance } from "fastify";
 import { enqueue } from "./queue";
-
+import { prisma } from "../prisma/lib/prismaClient";
+import { Status } from "../generated/prisma/enums";
 const jobSchema = {
   schema: {
     body: {
@@ -41,6 +42,38 @@ export function jobRoutes(fastify: FastifyInstance) {
       res.status(500).send(`Internal Server Error: ${err}`)
     }
   })
-  fastify.get("/job/:id", async (req, res) => { res.send("jobs sear") })
-  fastify.delete("/job/:id", async (req, res) => { res.send("job s") })
+  fastify.get<{ Params: { id: string } }>("/job/:id", async (req, res) => {
+    const userId = req.headers["x-userid"] as string;
+    if (!userId) {
+      return res.status(400).send({
+        msg: "userid not found , kindly add it!",
+      });
+    }
+    const { id } = req.params;
+    const job = await prisma.jobs.findFirst({
+      where: {
+        id,
+        userID: userId,
+      },
+    });
+    if (job?.userID != userId) {
+      return res.status(403).send({
+        error: "Wrong user id!",
+      });
+    }
+    if (!job) {
+      return res.status(404).send({
+        error: "job id is wrong!",
+      });
+    }
+    return res.status(200).send({
+      jobType: job.type,
+      jobID: job.id,
+      jobStatus: job.status,
+      updatedAt: job.UpdatedAt,
+    });
+  });
+  fastify.delete("/job/:id", async (req, res) => {
+    res.send("job s");
+  });
 }
